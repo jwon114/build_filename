53ff70581aaf4bdc5b372db22773e1fc7aecc5f1
jwon114 <jthgwong@gmail.com>
2017-03-07 11:36:19 +1100

    timestamp logic

--- build_filename.py
+++ build_filename.py
@@ -27,12 +27,13 @@ with open(VITRO_DATA_SRC, newline='') as vitroInput:
 		vitro_data_keys.append(row[0])
 
 def formatTimezone(dt, local_tz):
-	return datetime.strptime(dt, '%Y-%m-%d %H:%M:%S.%f').replace(microsecond=0).replace(tzinfo=pytz.utc).astimezone(local_tz).strftime('%d/%m/%Y %H:%M:%S')
+	return datetime.strptime(dt, '%Y-%m-%d %H:%M:%S.%f').replace(tzinfo=pytz.utc).astimezone(local_tz).strftime('%d/%m/%Y %H:%M:%S')
 
-with open(RESULTS_SRC, 'w', newline='') as csvfile:
+with open(RESULTS_SRC, 'w', newline='') as csvfile:	
 	print('Opening file for writing')
 	results = csv.writer(csvfile, delimiter=',')
 	print('Calculating...')
+	results.writerow(['MRN_EPISODE_NUMBER, GUID, IG_DATETIME, V_DATETIME_FORMATTED, V_DATETIME, ACT_ID'])
 	for vcode in vitro_data_keys:
 		if vcode in iguana_data_keys:
 			#list of indexes
@@ -47,10 +48,12 @@ with open(RESULTS_SRC, 'w', newline='') as csvfile:
 					itime = datetime.strptime(iguana_data[IGindex][2], '%d/%m/%Y %H:%M:%S')
 					vtime_formatted = formatTimezone(vitro_data[Vindex][2], local_tz)
 					vtime = datetime.strptime(vtime_formatted, '%d/%m/%Y %H:%M:%S')
-					if (itime == vtime) or (itime < vtime and vtime <= (itime + timedelta(seconds=1))):
+					max_time = itime + timedelta(seconds=1)
+					min_time = itime + timedelta(seconds=-1)
+					if (itime == vtime) or (itime > vtime and vtime <= max_time) or (vtime >= min_time and vtime <= max_time):
 						actID = vitro_data[Vindex][1]
 						vitroDT = vitro_data[Vindex][2]
-						results.writerow(iguana_data[IGindex] + [vtime] + [vitroDT] + [actID])
+						results.writerow(iguana_data[IGindex] + [vtime_formatted] + [vitroDT] + [actID])
 						match = True
 						break
 					
